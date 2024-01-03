fdb4450918e2e14cd2662e1ea3594b7d3ccb2987
aaleksiev <a.aleksiev@cmcmarkets.com>
2024-01-03 14:59:24 +0000

    [beers] Fix - wrong beer details request url

--- punkapi/src/main/java/com/aaleksiev/punkapi/model/request/BeerDetailsRequest.kt
+++ punkapi/src/main/java/com/aaleksiev/punkapi/model/request/BeerDetailsRequest.kt
@@ -2,5 +2,5 @@ package com.aaleksiev.punkapi.model.request
 
 import io.ktor.resources.Resource
 
-@Resource("/v2/beers/")
+@Resource("/v2/beers/{id}")
 internal data class BeerDetailsRequest(val id: Long)
