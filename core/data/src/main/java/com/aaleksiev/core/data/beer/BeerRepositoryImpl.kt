4b5c418573ab8fe2fbc083f4eec9e18ed807a8e6
aaleksiev <a.aleksiev@cmcmarkets.com>
2024-01-04 09:50:47 +0000

    [beers] Add a comment

--- core/data/src/main/java/com/aaleksiev/core/data/beer/BeerRepositoryImpl.kt
+++ core/data/src/main/java/com/aaleksiev/core/data/beer/BeerRepositoryImpl.kt
@@ -9,6 +9,13 @@ import javax.inject.Inject
 internal class BeerRepositoryImpl @Inject constructor(
   private val punkApi: PunkApi
 ) : BeerRepository {
+
+  /**
+   * Repository functions return flow because if persistent caching is required
+   * in the future, where cached data is emitted first
+   * and after that an API request is made and fresh data is emitted
+   * no change will be required in the consumer of this repository
+   */
   override fun beers(): Flow<Result<List<Beer>>> = flow {
     emit(
       runCatching { punkApi.beers() }
