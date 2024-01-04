d60b5553038f99fddd1ce6d47727d792d538fe13
aaleksiev <a.aleksiev@cmcmarkets.com>
2024-01-04 09:46:08 +0000

    [beers] Fix - make beer details card scrollable

--- beer/src/main/java/com/aaleksiev/beer/ui/beerdetails/BeerDetailsScreen.kt
+++ beer/src/main/java/com/aaleksiev/beer/ui/beerdetails/BeerDetailsScreen.kt
@@ -9,6 +9,8 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Card
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
@@ -89,7 +91,11 @@ private fun BeerDetails(
   modifier: Modifier = Modifier,
   beer: Beer
 ) {
-  Card(modifier = modifier.fillMaxWidth()) {
+  Card(
+    modifier = modifier
+      .fillMaxWidth()
+      .verticalScroll(state = rememberScrollState())
+  ) {
     Row(
       modifier = Modifier
         .padding(MaterialTheme.paddings.medium)
